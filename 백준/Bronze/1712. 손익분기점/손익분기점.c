d0368fb96d7b0aa8abc8186148fe9beca0765752
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2022-04-27 18:13:43 +0900

    [Bronze IV] Title: 손익분기점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (b >= c) {
+		printf("-1");
+		return 0;
+	}
+
+	int gap = c - b;
+
+	int count = a / gap;
+	count++;
+
+	printf("%d", count);
+}
