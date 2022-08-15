c4e4251706a612e9908d0563aa188903f721691c
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2022-08-15 20:03:36 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3036 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+vector<pair <int, int>> tree[10001];
+
+int visited[10001];
+int end_node = 0;
+int answer = 0;
+
+void dfs(int node, int distance) {
+
+    if (visited[node]) return;
+    visited[node] = true;
+
+    if (answer < distance) {// 길이와 마지막노드 갱신
+        answer = distance;
+        end_node = node;
+    }
+    
+    for (int i = 0;i < tree[node].size();i++){
+        int next = tree[node][i].first;
+        int next_dis = tree[node][i].second;
+        dfs(next, next_dis + distance);
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n = 0;
+    cin >> n;
+
+    int p, c, d = 0;
+    for (int i = 0; i < n - 1; i++){
+        cin >> p >> c >> d;
+
+        tree[p].push_back({c, d});
+        tree[c].push_back({p, d});
+    }
+    
+    dfs(1, 0);
+
+    memset(visited, false, sizeof(visited));
+
+    answer = 0;
+    dfs(end_node, 0);
+
+    cout << answer << "\n";
+    
+    return 0;
+}
