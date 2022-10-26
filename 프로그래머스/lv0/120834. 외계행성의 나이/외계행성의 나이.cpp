b0941a447adeee36f78bdc259eaa4bcc149f5dc8
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2022-10-27 06:48:12 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int age) {
+    string answer = "";
+    
+    while (age > 0){
+        answer = (char)('a' + (age % 10)) + answer; 
+        
+        age /= 10;
+    }
+    return answer;
+}
