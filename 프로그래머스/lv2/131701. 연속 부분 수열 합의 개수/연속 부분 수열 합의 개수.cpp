9fce88ac3e39b4d71c977e76642a4b083c68312d
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2023-02-26 23:36:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 342.99 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,32 +1,21 @@
 #include <string>
 #include <vector>
 #include <array>
-#include <iostream>
+#include <set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
     int answer = 0;
-    array<int, 1000001> arr = {0,};
+    set<int> s;
     
     for (int i = 0; i < elements.size(); i++) {
-        for (int count = 1; count <= elements.size(); count++) {//2
-            int get_count = 0;
-            int sum = 0;
-            while (get_count < count) {
-                int index = i + get_count;
-                
-                if (index > elements.size() - 1) index -= elements.size();
-                sum += elements[index];
-                
-                //cout << sum << " ";
-                if (!arr[sum]) arr[sum] = 1;
-                
-                get_count++;
-            }
+        int sum = 0;
+        for (int j = i; j < i + elements.size(); j++) {
+            sum += elements[j % elements.size()];
+            s.insert(sum);
         }
     }
-    
-    for (auto a : arr) if (a) answer++;
-    return answer;
+    return s.size();
 }
+
