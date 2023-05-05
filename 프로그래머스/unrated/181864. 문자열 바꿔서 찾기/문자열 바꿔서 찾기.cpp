257fd86216163cd881df0be6cdd2674cdeb0a9ad
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2023-05-05 20:54:22 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+
+    for (int i = 0; i < pat.length(); i++) {
+        if (pat[i] == 'A') pat[i] = 'B';
+        else pat[i] = 'A';
+    }
+    
+    return myString.find(pat) != string::npos ? 1 : 0;
+}
