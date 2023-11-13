adcb15fea49f77f0baaa590f327f7c7887902d12
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2023-11-13 13:20:56 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 88 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,28 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <map>
+
 using namespace std;
 
+struct compare {
+	bool operator()(string &a, string &b) {
+		return a > b;
+	}
+};
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	map<string, string, greater<>> work;
 
-    map<string, string, greater<>> m;
-    int n; cin >> n;
-    for (int i = 0; i < n; i++) {
-        string name, EorL;
+	int n = 0;
+	cin >> n;
 
-        cin >> name >> EorL;
-        m[name] = EorL;
 
-    }
+	for (int i = 0; i < n; i++) {
+		string name = "";
+		string state = "";
+		cin >> name >> state;
 
-    map<string, string>::iterator it;
-    for (it = m.begin(); it != m.end(); it++) {
-        if (it->second == "enter") {
-            cout << it->first << "\n";
-        }
-    }
+		work[name] = state;
+	}
 
-    return 0;
+	vector<string> data;
+	for (auto w : work) {
+		if (w.second == "enter") cout << w.first << "\n";
+	}
+	return 0;
 }
