06737e8fd8ebbfcb192c6f6c9b04eb54c7fa7560
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2024-09-01 22:09:35 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,28 +1,50 @@
 #include <iostream>
-#include <map>
-#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int main(void) {
-	ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+vector<int> arr;
 
-	map<int, bool> map;
-
-	int num;
-
-	int n, m;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		map.insert({ num, true });
+void search(int start, int end, int target) {
+	while (1) {
+		if (start == end) {// 마지막 원소일 때
+			if (arr[start] == target) cout << "1\n";
+			else cout << "0\n";
+			break;
+		}
+		int mid = (start + end) / 2;
+		if (arr[mid] < target) {
+			start = mid + 1;
+		}
+		else {
+			end = mid;
+		}
 	}
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		cin >> num;
-		if (map.count(num)) cout << "1\n";
-		else cout << "0\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N = 0, M = 0;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int temp = 0;
+		cin >> temp;
+		arr.push_back(temp);
+	}
+	sort(arr.begin(), arr.end());
+	
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		int t = 0;
+		cin >> t;
+
+		search(0, N - 1, t);
 	}
 }
