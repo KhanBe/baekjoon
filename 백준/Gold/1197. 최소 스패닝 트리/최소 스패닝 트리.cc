375d572f7b5182db30cbc68e7fe10b17488398e3
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2024-09-10 17:39:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 28 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -5,45 +5,78 @@
 
 using namespace std;
 
-int v = 0, e = 0;
-int min = 1000001;
-int visited[10001];
-int answer = 0;
-vector<pair<int, int>> list[10001];
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+int parent[10001];
+
+class Edge {
+private:
+	int node[2];
+	int weight;
+
+public:
+	Edge(int a, int b, int weight){
+		this->node[0] = a;
+		this->node[1] = b;
+		this->weight = weight;
+	}
+	int getNode(int x) {
+		return this->node[x];
+	}
+	int getWeight() {
+		return this->weight;
+	}
+	bool operator < (Edge& Edge) {
+		return this->weight < Edge.weight;
+	}
+};
+
+int find(int x) {//대표집합 찾기
+	if (parent[x] == x) return x;
+	return parent[x] = find(parent[x]);
+}
+
+void uni(int x, int y) {//같은 소속 만들기
+	x = find(x);
+	y = find(y);
+	if (x == y) return;//같은 소속이면
+	parent[y] = x;
+}
+
+bool sameParent(int x, int y) {
+	x = find(x);
+	y = find(y);
+	return x == y ? true : false;
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	cin >> v >> e;
-	for (int i = 0; i < e; i++) {//입력
-		int to, from, weight;
-		cin >> to >> from >> weight;
-		list[to].push_back({ weight, from });
-		list[from].push_back({ weight, to });
+	int E = 0;
+	int V = 0;
+	int answer = 0;
+
+	vector<Edge> edge;
+
+	cin >> V >> E;
+	for (int i = 0; i < E; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		edge.push_back(Edge(a, b, c));
 	}
-	
-	pq.push({ 0, 1 });
+	sort(edge.begin(), edge.end());
 
-	while (!pq.empty()) {
-		pair<int, int> cur = pq.top();
-		pq.pop();
-		if (visited[cur.second]) continue;
-		visited[cur.second] = true;
-		answer += cur.first;
+	for (int i = 1; i <= V; i++) parent[i] = i;
 
-		pair<int, int> next;
-
-		for (int i = 0; i < list[cur.second].size(); i++) {
-			next = list[cur.second][i];
-
-			if (visited[next.second]) continue;
-			pq.push(next);
-
+	for (int i = 0; i < edge.size(); i++) {
+		int x = edge[i].getNode(0);
+		int y = edge[i].getNode(1);
+		int w = edge[i].getWeight();
+		if (!sameParent(x, y)) {
+			uni(x, y);
+			answer += w;
 		}
 	}
-	
+
 	cout << answer;
 }
