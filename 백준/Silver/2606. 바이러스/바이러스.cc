af337e3c1a151be3c1f539e86eb82f2ac74adc14
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2024-10-05 20:53:30 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stack>
 #include <queue>
-
-#define INF 987654321
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-vector<int> node[101];
-bool visited[101];
+int n = 0;
+int m = 0;
+vector<int> list[101];
+int visited[101];
+int answer = 0;
+
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	visited[start] = true;
+
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < list[cur].size(); i++) {
+			int next = list[cur][i];
+
+			if (visited[next]) continue;
+
+			q.push(next);
+			visited[next] = true;
+			answer++;
+		}
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int n, m;
-	cin >> n >> m;
+	cin >> n;
+	cin >> m;
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
-		node[a].push_back(b);
-		node[b].push_back(a);
-	}
-	//============================
-
-	queue<int> q;
-
-	q.push(1);
-	visited[1] = true;
-
-	while (!q.empty()) {
-		int curNode = q.front();
-		q.pop();
-		
-		for (int i = 0; i < node[curNode].size(); i++) {
-			int nextNode = node[curNode][i];
-			if (!visited[nextNode]) {
-				visited[nextNode] = true;
-				q.push(nextNode);
-			}
-		}
+		list[a].push_back(b);
+		list[b].push_back(a);
 	}
 
-	int answer = -1;
-	for (auto v : visited) answer += v;
+	bfs(1);
 
 	cout << answer;
-	return 0;
 }
