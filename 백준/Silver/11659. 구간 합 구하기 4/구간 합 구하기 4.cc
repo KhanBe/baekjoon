a570eb820a81ce376815acb959a560388b88cdef
jaehak Woo <61501112+KhanBe@users.noreply.github.com>
2025-08-09 00:06:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,37 +1,29 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-	int sum = 0;
-	vector<int> sums;
+    int arr[100001];
+    
+    int n = 0, m = 0;
+    cin >> n >> m;
 
-	int n, m;
-	cin >> n >> m;
-	
-
-	int num = 0;
-	for (int i = 0; i < n; i++) { //O(n)
-		cin >> num;
-		sum += num;
-		sums.push_back(sum);
-	}
-
-	// 원래는 그냥 각 숫자들의 배열로 합을 구함O(n^2)
-	// 시간 초과로 아예 합들의 배열로 바꿔 O(n)으로 구현해봄
-	int a, b;
-	for (int i = 0; i < m; i++) {
-		sum = 0;
-		cin >> a >> b;
-
-		sum = sums[b - 1];
-		if (a != 1) sum -= sums[a - 2];
-
-		cout << sum << "\n";
-	}
+    int temp = 0;
+    for (int i = 1; i <= n ; i++){
+        cin >> temp;
+        arr[i] = arr[i - 1] + temp;
+    }
+    
+    int a = 0, b = 0;
+    for (int i =0;i <m;i++) {
+        cin >> a >> b;
+        cout << arr[b] - arr[a - 1] << "\n";
+    }
+    
+    return 0;
 }
